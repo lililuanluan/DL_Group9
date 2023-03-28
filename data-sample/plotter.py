51e30c3cd3f27bd16fee45194199bfd5ec4ddafe
luanli <L.Li-30@student.tudelft.nl>
2023-03-29 04:26:51 +0800

    plot all figures

--- data-sample/plotter.py
+++ data-sample/plotter.py
@@ -1,14 +1,18 @@
 import nibabel as nib
 import numpy as np
 import matplotlib.pyplot as plt
+from os import walk
+import glob
+path = "*.mgz"
+files = glob.glob(path)
+for path in files:
+    print("filename=",path)
+    X = nib.load(path)
+    X = X.get_fdata()
+    print("X.shape=",np.shape(X))
 
-path = "./003.mgz"
-X = nib.load(path)
-X = X.get_fdata()
-print("X.shape=",np.shape(X))
-
-for i in range(25):
-    plt.subplot(5, 5, i + 1)
-    plt.imshow(X[:,:,5*i])
-    plt.gcf().set_size_inches(10, 10)
-plt.show()
+    for i in range(25):
+        plt.subplot(5, 5, i + 1)
+        plt.imshow(X[:,:,5*i])
+        plt.gcf().set_size_inches(10, 10)
+    plt.show()
